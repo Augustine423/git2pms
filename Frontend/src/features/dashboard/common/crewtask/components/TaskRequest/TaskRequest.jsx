0e1eb922c1c927f7bb945f7e9ee26a22febf9a90
Kyaw Zay Yar Soe <kyawzayarsoe@gmail.com>
2025-06-30 14:32:14 +0630

    updated

--- Frontend/src/features/dashboard/common/crewtask/components/TaskRequest/TaskRequest.jsx
+++ Frontend/src/features/dashboard/common/crewtask/components/TaskRequest/TaskRequest.jsx
@@ -24,7 +24,7 @@ const TaskRequest = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://192.168.0.166:8080/mdt/task/register", {
+      const response = await fetch("http://pms.aioceaneye.com:8080/mdt/task/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
